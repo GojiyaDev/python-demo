594ed586738dda24ae52d4905aff9b516ca1b812
GojiyaDev <DEV.17103@stu.upes.ac.in>
2024-08-27 18:40:57 +0530

    Adding more Program in SecondFile-27-08-2024.py
    P3: - Function Which print ID and Type of user input
    P4: - String Concatenation
    P5: - Take Number from User and return Whole Table of that Number

--- SecondFile-27-08-2024.py
+++ SecondFile-27-08-2024.py
@@ -27,3 +27,44 @@ elif b > a and b > c:
     print("B is Largest")
 else:
     print("C is Largest")
+
+#function can be use multiple time 
+""" 
+def is short for definition
+"""
+
+def myprint(inputvar):
+    a=inputvar
+    print(f"value is {a}")
+    print(f"id is {id(a)}")
+    print(f"Type is {type(a)}")
+
+myprint(10)
+myprint(1)
+myprint(100)
+
+#write a program user input first name and last name and great with like hii user and his name should be upper case
+
+firstname = input("Enter Your First Name :")
+lastname = input("Enter Your Last Name : ")
+
+firstname = firstname.capitalize()
+lastname = lastname.capitalize()
+
+fullname = "Hello "+firstname +" "+ lastname
+print(fullname)
+
+
+while True:
+    try:        
+        number = int(input("Enter an integer: "))
+        break  
+    except ValueError:
+        
+        print("Invalid input. Please enter a valid integer.")
+
+
+print(f"\n Multiplication Table for {number}:")
+for i in range(1, 11): 
+    result = number * i
+    print(f"{number} x {i} = {result}")
