2343dc55e6ac7f714c1686f6120d1284f31aec0b
GojiyaDev <DEV.17103@stu.upes.ac.in>
2024-08-27 18:34:54 +0530

    Python Code File Date :- 27-08-2024
    P1: - Conditional Statement
    P2:-  Operator like And, Or, Not

---
+++ SecondFile-27-08-2024.py
@@ -0,0 +1,29 @@
+# 27-08-2024 time 3:00 to 3:55 in 4001
+# if block
+a=10
+b=1
+if a > b:
+    print("A > B")
+    print("this condition is true")
+if a < b:
+    print("A < B")
+    print("this condition is true")
+if a == b:
+    print("A == B")
+    print("this condition is true")
+if a != b:
+    print("A != B")
+    print("this condition is true")
+
+
+# And, OR, Not operator
+a = 100
+b = 2
+c = 30
+
+if a > b and a > c:
+    print("A is Largest")
+elif b > a and b > c:
+    print("B is Largest")
+else:
+    print("C is Largest")
